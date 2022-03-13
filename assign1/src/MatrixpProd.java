cfcfa232bd7672f471d6fabe256fde1e49f151f6
up201804977 <up201804977@alunos.fc.up.pt>
2022-03-13 18:06:22 +0000

    LineMultiplication- Java

--- assign1/src/MatrixpProd.java
+++ assign1/src/MatrixpProd.java
@@ -9,18 +9,18 @@ class Matrixm{
         double temp;
         int i, j, k;
     
-        double []pha, phb, phc;
-        pha= new double[m_ar*m_br];
-        phb= new double[m_ar*m_br];
-        phc= new double[m_ar*m_br];
+        double [][]pha, phb, phc;
+        pha= new double[m_ar][m_br];
+        phb= new double[m_ar][m_br];
+        phc= new double[m_ar][m_br];
     
         for(i=0; i<m_ar; i++)
             for(j=0; j<m_ar; j++)
-                pha[i*m_ar + j] =1.0;
+                pha[i][j] =1.0;
     
         for(i=0; i<m_br; i++)
             for(j=0; j<m_br; j++)
-                phb[i*m_br + j] = i+1;
+                phb[i][j] = i+1;
     
     
     
@@ -31,15 +31,15 @@ class Matrixm{
             {	temp = 0;
                 for( k=0; k<m_ar; k++)
                 {	
-                    temp += pha[i*m_ar+k] * phb[k*m_br+j];
+                    temp += pha[i][k] * phb[k][j];
                 }
-                phc[i*m_ar+j]=temp;
+                phc[i][j]=temp;
             }
         }
     
     
         Time2 = System.currentTimeMillis();
-        st= String.format("Time: %3.3f seconds\n", (double)(Time2 - Time1)/1000000);
+        st= String.format("Time: %3.3f seconds\n", (double)(Time2 - Time1)/1000);
         System.out.println(st);
         //System.out.printf("Time: %3.3f seconds\n", (double)(Time2 - Time1)/1000000);
      
@@ -47,7 +47,7 @@ class Matrixm{
         System.out.println("Result matrix:");
         for(i=0; i<1; i++)
         {	for(j=0; j<Math.min(10,m_br); j++)
-                System.out.print(phc[j]+" ");
+                System.out.print(phc[i][j]+" ");
         }
         System.out.println();
     }
@@ -55,7 +55,61 @@ class Matrixm{
     // add code here for line x line matriz multiplication
     static void OnMultLine(int m_ar, int m_br)
     {
+        long Time1, Time2;
+	
+        String st;
+        double temp;
+        int i, j, k;
     
+        double [][]pha, phb, phc;
+        pha= new double[m_ar][m_br];
+        phb= new double[m_ar][m_br];
+        phc= new double[m_ar][m_br];
+    
+        for(i=0; i<m_ar; i++)
+            for(j=0; j<m_ar; j++)
+                pha[i][j] =1.0;
+    
+        for(i=0; i<m_br; i++)
+            for(j=0; j<m_br; j++)
+                phb[i][j] = i+1;
+    
+    
+    
+        Time1 = System.currentTimeMillis();
+    
+        for(i=0; i<m_ar; i++){
+            
+            for( j=0; j<m_br; j++){
+                //a[i][j]
+                temp = 0;
+                   
+                for(int l=0;l<m_ar;l++){
+
+                    for(int m=0;m<m_ar;m++){
+                        phc[m][i]+=pha[l][j]*phb[m][l];
+                        //System.out.println("matrix a["+l+"]"+"["+j+"] * matrix b["+m+"]"+"["+l+"] += matrix c["+m+"]"+"["+i+"] ");
+                    }
+                    //m certo
+                }
+                
+                
+            }
+        }
+    
+    
+        Time2 = System.currentTimeMillis();
+        st= String.format("Time: %3.3f seconds\n", (double)(Time2 - Time1)/1000);
+        System.out.println(st);
+        //System.out.printf("Time: %3.3f seconds\n", (double)(Time2 - Time1)/1000000);
+     
+        // display 10 elements of the result matrix tto verify correctness
+        System.out.println("Result matrix:");
+        for(i=0; i<1; i++)
+        {	for(j=0; j<Math.min(10,m_br); j++)
+                System.out.print(phc[i][j]+" ");
+        }
+        System.out.println();
     
     }
 
@@ -64,7 +118,7 @@ class Matrixm{
     {
     
     
-}
+    }
     public static void main(String args[]){
         Scanner scanner = new Scanner(System.in);
         int op=1;
