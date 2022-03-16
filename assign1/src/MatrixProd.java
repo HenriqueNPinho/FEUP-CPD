d2b0ad118be031cf57473194cf43408b262a5875
Luis <up201104354@fe.up.pt>
2022-03-16 17:30:22 +0000

    improved java algorithm

--- assign1/src/MatrixProd.java
+++ assign1/src/MatrixProd.java
@@ -6,39 +6,42 @@ class MatrixProd{
         System.out.printf("Time: %3.3f seconds\n", (double)(Time2 - Time1) / 1000);
     }
     
-    static void printMatrix(double phc[][], int m_br){
+    static void printMatrix(double phc[], int m_br){
 
         System.out.println("Result matrix: ");
         for(int i=0; i<1; i++)
         {	for(int j=0; j<Math.min(10,m_br); j++)
-                System.out.print(phc[i][j]+ " ");
+                System.out.print(phc[j]+ " ");
         }
         System.out.println();
     }
     
     static void OnMult(int m_ar, int m_br){
         long Time1, Time2;
+        double temp;
         int i, j, k;
 
-        double [][]pha= new double[m_ar][m_br];
-        double [][]phb= new double[m_ar][m_br];
-        double [][]phc= new double[m_ar][m_br];
+        double []pha, phb, phc;
+        pha= new double[m_ar*m_br];
+        phb= new double[m_ar*m_br];
+        phc= new double[m_ar*m_br];
     
         for(i=0; i<m_ar; i++){
             for(j=0; j<m_br; j++){
-                pha[i][j] =1.0;
-                phb[i][j] = i+1;
+                pha[i*m_ar + j] =1.0;
+                phb[i*m_br + j] =i+1.0;
             }
         }
     
         Time1 = System.currentTimeMillis();
         for(i=0; i<m_ar; i++)
         {	for( j=0; j<m_br; j++)
-            {
+            {	temp = 0;
                 for( k=0; k<m_ar; k++)
                 {	
-                    phc[i][j]+= pha[i][k] * phb[k][j];
+                    temp += pha[i*m_ar+k] * phb[k*m_br+j];
                 }
+                phc[i*m_ar+j]=temp;
             }
         }
         Time2 = System.currentTimeMillis();
@@ -53,29 +56,28 @@ class MatrixProd{
         long Time1, Time2;
         int i, j, k;
     
-        double [][]pha= new double[m_ar][m_br];
-        double [][]phb= new double[m_ar][m_br];
-        double [][]phc= new double[m_ar][m_br];
+        double []pha, phb, phc;
+        pha= new double[m_ar*m_br];
+        phb= new double[m_ar*m_br];
+        phc= new double[m_ar*m_br];
     
         for(i=0; i<m_ar; i++){
             for(j=0; j<m_br; j++){
-                pha[i][j] =1.0;
-                phb[i][j] = i+1;
+                pha[i*m_ar + j] = 1.0;
+                phb[i*m_ar + j] = i+1.0;
             }
         }
     
         Time1 = System.currentTimeMillis();
-        for(i=0; i<m_ar; i++){
-            
-            for( j=0; j<m_br; j++){
-                   
-                for(k=0;k<m_ar;k++){
-
-                    phc[i][k] += pha[i][j]*phb[j][k];
+        for(i=0; i<m_ar; i++)
+        {	for( j=0; j<m_br; j++)
+            {	double pha_aux = pha[i*m_ar+j];
+                for( k=0; k<m_ar; k++)
+                {	
+                    phc[i*m_ar+k] += pha_aux * phb[j*m_br+k];
                 }
-                
             }
-        }
+	    }
         Time2 = System.currentTimeMillis();
         
         printTime(Time1, Time2);
@@ -91,7 +93,6 @@ class MatrixProd{
         do{
             System.out.println("1. Multiplication");
             System.out.println("2. Line Multiplication");
-            System.out.println("3. Block Multiplication");
             System.out.print("Selection: "); op=scanner.nextInt();
 
             System.out.print("Dimensions: lins=cols ? ");
